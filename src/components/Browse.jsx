f72ef69b7b7d37477ebd75ed388978eb4f825ca6
warriorBunny013 <mona23sonai@gmail.com>
2023-07-12 16:31:19 +0530

    completed

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -11,35 +11,53 @@ const Browse = () => {
   console.log(data)
     return (
         <div className="bg-white w-[27rem] h-[45rem] ">
-        <div className=" px-10 py-4 font-medium tracking-wide">Select Dishes</div>
+        <div className="py-4 px-5 font-medium tracking-wide flex"><img className="w-[0.4rem] mt-[0.45rem] mr-[1.5rem] h-[0.7rem]" src="leftarrow.png" alt="arrow"/><span>Select Dishes</span></div>
         <div>
           <div className="bg-slate-800 h-10 pt-5">
-            <div className="flex bg-white shadow-sm justify-between text-slate-800 font-bold mx-5 text-sm px-4 py-4 rounded-md">
-              <div className="border-r pr-6">21 May 2021</div>
-              <div>10:30 Pm-12:30 Pm</div>
+            <div className="flex bg-white shadow-md  justify-between text-slate-800 font-semibold mx-5  px-4 py-5 rounded-md">
+              <div className="flex">
+                <img className="w-[0.8rem] h-[0.8rem] mt-[0.2rem] mr-[0.4rem]" alt="calender" src="calender.png"/>
+                <span className="text-[0.7rem]">21 May 2021</span>
+                
+                </div>
+              
+                <div className="flex">
+                <img className="w-[0.8rem] h-[0.8rem] mt-[0.2rem] mr-[0.4rem]" alt="clock" src="alarm.png"/>
+                <span className="text-[0.7rem]">10:30 Pm-12:30 Pm</span>
+                
+                </div>
             </div>
           </div>
-          <div className="mt-14 mx-5 flex overflow-hidden gap-4">
-            <button className="bg-yellow-100 rounded-xl px-4  border-2 border-yellow-400 text-[0.8rem] text-yellow-400 font-bold">Italian</button>
-            <button className=" rounded-xl px-4  border-2  text-[0.8rem]  border-slate-300 text-slate-300 ">Italian</button>
-            <button className=" rounded-xl px-4  border-2 border-slate-300 text-slate-300  text-[0.8rem]">Italian</button>
+          <div className="mt-14 mx-5 flex overflow-hidden gap-4 text-[0.6rem]">
+            <button className="bg-yellow-100 rounded-xl px-6 py-1 border  border-yellow-400 text-[0.6rem] text-yellow-400 font-semibold">Italian</button>
+            <button className=" rounded-xl px-6 py-1   border text-[0.6rem]  border-slate-300 text-slate-400 ">Indian</button>
+            <button className=" rounded-xl px-6 py-1 border  border-slate-300 text-slate-400  text-[0.6rem]">Indian</button>
             {/* <button className=" rounded-xl px-4  border-2  text-[0.8rem]  font-bold">Italian</button> */}
           </div>
         </div>
         <div>
-          <div className="font-bold tracking-wide mx-5 mt-5 text-slate-800">Popular Dishes</div>
-          <div className="flex justify-center border-b-2  mb-5">
-          <div className="flex mb-5  mt-5  w-[20rem] [375px]:w-[27rem] overflow-x-auto hide-scroll-bar space-x-4">
+          <div className="font-bold tracking-wide mx-5 mt-5  text-sm text-slate-800">Popular Dishes</div>
+          <div className="flex justify-center  ">
+          <div className="flex mb-5  mt-2  max-w-[27rem]  overflow-x-auto hide-scroll-bar space-x-4">
             {data?.popularDishes?.map((data,i)=>{
               return    <><section className="relative flex-shrink-0 ml-5 rounded-full border-2 border-yellow-400">
-              <span><img src={data.image} className="bg-yellow-300 h-14 w-14 rounded-full border-2 border-white" alt=""
-              /></span>
-              <div className="absolute top-5 left-3 text-white text-[0.6rem]">{data.name}</div>
-            </section><section className="relative flex-shrink-0 ml-5 rounded-full border-2 border-yellow-400">
-              <span><img src={data.image} className="bg-yellow-300 h-14 w-14 rounded-full border-2 border-white" alt=""
-              /></span>
-              <div className="absolute top-5 left-3 text-white text-[0.6rem]">{data.name}</div>
-            </section></>
+              <span className="relative ">
+                <img src={data.image} className=" bg-yellow-300 object-cover h-14 w-14 rounded-full border-2 border-white" alt=""
+              />
+              <div className="absolute top-0 z-10 glass-body rounded-full h-14 w-14 border-2 border-white "></div>
+              </span>
+              <div className="absolute top-5 left-3 text-white text-[0.6rem] z-10">{data.name}</div>
+            </section>
+            <section className="relative flex-shrink-0 ml-5 rounded-full border-2 border-yellow-400">
+              <span className="relative ">
+                <img src={data.image} className=" bg-yellow-300 object-cover h-14 w-14 rounded-full border-2 border-white" alt=""
+              />
+              <div className="absolute top-0 z-10 glass-body rounded-full h-14 w-14 border-2 border-white "></div>
+              </span>
+              <div className="absolute top-5 left-3 text-white text-[0.6rem] z-10">{data.name}</div>
+            </section>
+            
+            </>
             })}
           {/* <div className="bg-pink-500 rounded-full flex items-center justify-center text-white text-xs h-[3.6rem] w-[3.6rem]">Biryani</div> */}
    
@@ -48,51 +66,86 @@ const Browse = () => {
         </div>
         <div>
         
-          <div className="flex justify-between mb-3 mx-5">
-            <div className="font-bold tracking-wide  text-slate-800 text-xl">Recomended</div>
-            <button className="bg-slate-900 text-white text-[0.6rem] px-4 py-1 rounded-md">Menu</button></div>
-          <div className="mx-5 flex flex-col space-y-2 h-[20rem] overflow-y-auto hide-scroll-bar">
+         
+          <div className="mx-5 flex flex-col pt-4 border-t-2  h-[20rem] overflow-y-auto hide-scroll-bar">
+          <div className="flex justify-between">
+            <div className="font-bold tracking-wide flex  text-slate-800 text-lg">
+              <span>Recommended</span>
+              <img className="w-[0.5rem] h-[0.3rem] mt-3 ml-3" src="arrowdown.png" alt="ksnf"/>
+              </div>
+            <button className="bg-slate-900 text-white text-[0.6rem] px-5 h-[1.4rem] shadow-md rounded-md">Menu</button>
+          </div>
+          <div className="">
             {data?.dishes?.map((dat,i)=>{
-              return <><div key={i} className=" flex py-4 justify-between">
+              return <><div key={i} className=" flex py-5 border-b justify-between">
               <div className="">
-              <div className="flex gap-2 font-semibold text-md mb-1"><div>{dat?.name}</div><div className="flex"><img className="w-[0.6rem] h-[0.6rem] mt-[0.66rem]" alt="veg" src="veg.jpg"/></div><div><span className="bg-green-400 text-white px-1 rounded-sm text-[0.56rem]">{dat.rating}</span></div></div>
-              <div className="flex"><div className="flex gap-2 ">
+              <div className="flex gap-2 font-semibold text-xs mb-1">
+                <div>{dat?.name}</div>
+                <div className="flex flex-col justify-center">
+                  <img className="w-[0.4rem] h-[0.4rem] " alt="veg" src="veg.jpg"/>
+                </div>
+                <div>
+                  <span className="bg-green-500 text-white px-1 rounded-sm text-[0.4rem]">
+                    {dat?.rating}</span>
+                </div>
+              </div>
+              <div className="flex "><div className="flex gap-2 border-r pr-3">
               {dat?.equipments?.map((d,index)=>{
-                 return <span key={index} className="text-[0.46rem]">{d}</span>
+                 return<div className="flex flex-col"> 
+                  <img alt="appliances" className="w-[0.7rem] h-[1rem]" src="fridge.png"/>
+                  <span key={index} className="text-[0.4rem]">{d}</span>
+                  </div>
               })}
                 </div>
-              <div className=" border-l border-slate-300 flex flex-col px-4"><span className="text-[0.5rem] font-bold">Ingredients</span><Link to="/details" className="text-yellow-500 font-bold text-[0.6rem]">View list</Link></div></div>
-              <div className="text-[0.7rem] w-[13rem] leading-tight mt-2 text-slate-400 font-base">{dat.description}</div>
+              <div className=" flex flex-col px-4"><span className="text-[0.4rem] font-bold">Ingredients</span><Link to="/details" className="text-yellow-500 font-semi-bold text-[0.4rem]">View list</Link></div></div>
+              <div className="text-[0.56rem] w-[13rem] leading-tight mt-2 text-slate-500 ">{dat?.description}</div>
               </div>
               <div>
                 <div className=" w-[6rem] h-[4rem] relative rounded-lg">
-                 <img className="w-[6rem] h-[4rem] object-cover bg-red-700 rounded-lg" src={dat.image} alt="food"/>
-                 <button className="text-yellow-500 border-2 bg-white  border-yellow-400 shadow-sm px-4 absolute -bottom-3 left-5 font-medium text-sm  rounded-md">Add</button>
+                 <img className="w-[6rem] h-[4rem] object-cover bg-red-700 rounded-lg" src={dat?.image} alt="food"/>
+                 <button className="text-yellow-500 border-2 bg-white  border-yellow-400  px-5 absolute -bottom-3 left-5 font-medium text-xs py-[0.2rem]  rounded-md shadow-lg">Add</button>
                 </div>
               </div>
               </div>
-              <div key={i} className=" flex py-4 justify-between">
+              <div key={i} className=" flex py-5 border-b justify-between">
               <div className="">
-              <div className="flex gap-2 font-semibold text-md mb-1"><div>{dat?.name}</div><div><img className="w-[0.6rem] h-[0.6rem] mt-[0.66rem]" alt="veg" src="veg.jpg"/></div><div><span className="bg-green-400 text-white px-1 rounded-sm text-[0.56rem]">{dat.rating}</span></div></div>
-              <div className="flex"><div className="flex gap-2">
+              <div className="flex gap-2 font-semibold text-xs mb-1">
+                <div>{dat?.name}</div>
+                <div className="flex flex-col justify-center">
+                  <img className="w-[0.4rem] h-[0.4rem] " alt="veg" src="veg.jpg"/>
+                </div>
+                <div>
+                  <span className="bg-green-500 text-white px-1 rounded-sm text-[0.4rem]">
+                    {dat?.rating}</span>
+                </div>
+              </div>
+              <div className="flex "><div className="flex gap-2 border-r pr-3">
               {dat?.equipments?.map((d,index)=>{
-                 return <span key={index} className="text-[0.46rem]">{d}</span>
+                 return<div className="flex flex-col"> 
+                  <img alt="appliances" className="w-[0.7rem] h-[1rem]" src="fridge.png"/>
+                  <span key={index} className="text-[0.4rem]">{d}</span>
+                  </div>
               })}
                 </div>
-              <div className=" border-l border-slate-300 flex flex-col px-4"><span className="text-[0.5rem] font-bold">Ingredients</span><div className="text-yellow-500 font-bold text-[0.6rem]">View list</div></div></div>
-              <div className="text-[0.7rem] w-[13rem] leading-tight mt-2 text-slate-400 font-base">{dat.description}</div>
-              </div> 
+              <div className=" flex flex-col px-4"><span className="text-[0.4rem] font-bold">Ingredients</span><Link to="/details" className="text-yellow-500 font-semi-bold text-[0.4rem]">View list</Link></div></div>
+              <div className="text-[0.56rem] w-[13rem] leading-tight mt-2 text-slate-500 ">{dat?.description}</div>
+              </div>
               <div>
                 <div className=" w-[6rem] h-[4rem] relative rounded-lg">
-                 <img className="w-[6rem] h-[4rem] object-cover bg-red-700 rounded-lg" src={dat.image} alt="food"/>
-                 <button className="text-yellow-500 border-2 bg-white  border-yellow-400 shadow-sm px-4 absolute -bottom-3 left-5 font-medium text-sm  rounded-md">Add</button>
+                 <img className="w-[6rem] h-[4rem] object-cover bg-red-700 rounded-lg" src={dat?.image} alt="food"/>
+                 <button className="text-yellow-500 border-2 bg-white  border-yellow-400  px-5 absolute -bottom-3 left-5 font-medium text-xs py-[0.2rem]  rounded-md shadow-lg">Add</button>
                 </div>
               </div>
               </div>
+         
+              
+              
+              
               
               </>
             })}
-          
+            </div>
+            
           </div>
           
         </div>
--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -12,33 +12,46 @@ const Details = () => {
     console.log(details)
     return (
         <div className='bg-white w-[27rem] h-[45em]'>
-            <Link to="/" className='px-5 py-4 flex justify-between font-medium font-mono tracking-wide'>{"<"}</Link>
+            <Link to="/" className='px-5 py-4 flex justify-between font-medium font-mono tracking-wide'><img className="w-[0.4rem] mt-[0.45rem] mr-[1.5rem] h-[0.7rem]" src="leftarrow.png" alt="arrow"/></Link>
             <div className='ml-5 mt-5  flex justify-between'>
-            <div className=' pb-10 '>
+            <div className=' '>
                 <div className=''>
-            <div className='flex gap-2'><span className='text-xl whitespace-nowrap font-bold mb-2'>{details?.name}</span><div className='mt-1'><span className='bg-green-400 text-white px-1 rounded-sm text-[0.56rem]'>4.2</span></div></div>
-            <div className='text-[0.7rem] tracking-wider text-slate-400 font-medium'>Mughlai Masala is a style of cookery developed in the indian Subcontinent by the imperial kitchens of the Mughal Empire.</div>
+            <div className='flex gap-2'><span className='text-xl whitespace-nowrap font-bold mb-2'>{details?.name}</span><div className='mt-1'><span className='bg-green-400 text-white px-1 rounded-sm text-[0.4rem]'>4.2</span></div></div>
+            <div className='text-[0.5rem]  tracking-wide text-slate-300 '>Mughlai Masala is a style of cookery developed in the indian Subcontinent by the imperial kitchens of the Mughal Empire.</div>
             </div>
-            <div className='text-xs pb-5 pt-3 border-b-2 font-medium'>{details?.timeToPrepare}</div>
+            <div className='text-xs pb-5 pt-3 border-b-2 flex font-medium'>
+                <img alt="clock" className="w-[0.7rem] h-[0.7rem] mt-[0.2rem] mr-2" src="clock.png" />
+                <span className="text-[0.6rem]">{details?.timeToPrepare}</span>
+                
+                </div>
             </div>
-            <img className="h-[10rem]  w-[7.7rem] object-fill" src='food.png' alt='details' />
-
+            {/* <div className="bg-blue-400 rounded-full w-[20rem] h-[20rem]"></div> */}
+            <div className="relative">
+               <img className="max-h-[20rem] max-w-[10rem]" src='food.png' alt='details' />
+               <div className="opacity-25 bg-yellow-200 absolute -top-10  h-[14rem] w-[14rem] rounded-full"></div>
+                
+            </div>
+            
             </div>
             <div className='mx-5  pb-5 border-b flex flex-col'>
-                <span className='text-lg font-bold tracking-wide mb-1'>Ingredients</span>
-                <span className='text-[0.7rem] tracking-wider text-slate-400 font-medium'>For 2 people</span>
+                <span className='text-md font-bold tracking-wide '>Ingredients</span>
+                <span className='text-[0.5rem] tracking-wide text-slate-400 '>For 2 people</span>
             </div>
             <div className='pt-3 mx-5 '>
-                <div className='font-medium text-sm'>Vegetables(05)</div>
-                <div className='flex justify-between text-[0.75rem] text-slate-600'>
+                <div className='font-semibold flex text-xs'>
+                    <span>Vegetables(05)</span>
+                    <img src="arrowdown.png" className="w-[0.5rem] h-[0.3rem] mt-[0.3rem] ml-3" alt="arrowdown"/>
+                    
+                    </div>
+                <div className='flex justify-between text-[0.6rem] text-slate-600'>
                     <div className='flex flex-col gap-1 mt-2'>
                         {details?.ingredients?.vegetables?.map((data,i)=>{
-                            return <div key={i}>{data.name}</div>
+                            return <div key={i}>{data?.name}</div>
                         })}
                     </div>
                     <div className='flex flex-col gap-1 mt-2'>
                     {details?.ingredients?.vegetables.map((data,i)=>{
-                            return <div key={i}>{data.quantity}</div>
+                            return <div key={i}>{data?.quantity}</div>
                         })}
                     </div>
                 </div>
@@ -46,16 +59,20 @@ const Details = () => {
             </div>
            
             <div className='pt-5 mx-5 '>
-                <div className='font-medium text-sm'>Spices(10)</div>
-                <div className='flex justify-between text-[0.75rem] text-slate-600'>
+            <div className='font-semibold flex text-xs'>
+                    <span>Spices (10)</span>
+                    <img src="arrowdown.png" className="w-[0.5rem] h-[0.3rem] mt-[0.3rem] ml-3" alt="arrowdown"/>
+                    
+                    </div>
+                <div className='flex justify-between text-[0.6rem] text-slate-600'>
                     <div className='flex flex-col gap-1 mt-2'>
                     {details?.ingredients?.spices.map((data,i)=>{
-                            return <div key={i}>{data.name}</div>
+                            return <div key={i}>{data?.name}</div>
                         })}
                     </div>
                     <div className='flex flex-col gap-1 mt-2'>
                     {details?.ingredients?.spices.map((data,i)=>{
-                            return <div key={i}>{data.quantity}</div>
+                            return <div key={i}>{data?.quantity}</div>
                         })}
                     </div>
                 </div>
@@ -63,13 +80,13 @@ const Details = () => {
             </div>
            
             
-            <div className=' mx-5 '>
-                <div className='text-lg font-bold tracking-wide py-4'>Appliances</div>
+            <div className=' mx-5 mt-5 '>
+                <span className='text-md font-bold tracking-wide '>Appliances</span>
                 <div className='flex gap-6 text-[0.6rem] '>
                 {details?.ingredients?.appliances?.map((data,i)=>{
-                            return  <div key={i} className='bg-slate-200 overflow-auto object-cover px-4 flex flex-col justify-between py-2'>
-                            <img src="refrigerator.png" className="h-[7rem]" alt='appliances'/>
-                            <span className="">{data.name}</span>
+                            return  <div key={i} className='bg-slate-100 mt-2 overflow-auto object-cover px-4 flex flex-col justify-between py-2'>
+                            <img src="refrigerator.png" className="w-[2rem]" alt='appliances'/>
+                            <span className="">{data?.name}</span>
                         </div>
                         })}
                 </div>
